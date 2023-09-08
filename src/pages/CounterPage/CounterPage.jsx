9ccc36f11153c6249462ecb8f8916cf3300cefa8
azozrazoz <doshan.de@gmail.com>
2023-09-08 20:24:48 +0600

    в проект добавлен reduxToolkit

---
+++ src/pages/CounterPage/CounterPage.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import s from "./CounterPage.module.scss";
+import { addBy, decrement, increment } from "../../store/counterSlice";
+
+function CounterPage() {
+    const dispatch = useDispatch()
+  const { count } = useSelector((state) => state.counter);
+
+  const handlerIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  const handleAddByTen = () => {
+    dispatch(addBy(10));
+  };
+
+  return (
+    <div>
+      <div>{count}</div>
+      <div>
+        <button
+          type="button"
+          onClick={handleDecrement}
+          style={{
+            padding: "20px",
+            margin: "20px",
+            backgroundColor: "lightgray",
+          }}
+        >
+          -1
+        </button>
+        <button
+          type="button"
+          onClick={handlerIncrement}
+          style={{
+            padding: "20px",
+            margin: "20px",
+            backgroundColor: "lightgray",
+          }}
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          onClick={handleAddByTen}
+          style={{
+            padding: "20px",
+            margin: "20px",
+            backgroundColor: "lightgray",
+          }}
+        >
+          +10
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CounterPage;

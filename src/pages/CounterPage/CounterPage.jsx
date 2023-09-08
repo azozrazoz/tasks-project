190fd1e3b3635c455c6719bd331a4fb01d88d355
azozrazoz <doshan.de@gmail.com>
2023-09-08 20:44:45 +0600

    добавлен новый slice в хранилище tasksSlice и подключен к проекту

--- src/pages/CounterPage/CounterPage.jsx
+++ src/pages/CounterPage/CounterPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import s from "./CounterPage.module.scss";
+// import s from "./CounterPage.module.scss";
 import { addBy, decrement, increment } from "../../store/counterSlice";
 
 function CounterPage() {
---
+++ src/store/tasksSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const tasksSlice = createSlice({
+    name: "tasks",
+    initialState: {
+        tasks: [],
+    },
+    reducers: {
+
+    }
+});
+
+export const tasksReducer = tasksSlice.reducer;
